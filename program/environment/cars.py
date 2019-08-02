13adec2e6a809a6f30276a47c59b76d46d3acf84
Davanchama <davanchama@gmail.com>
2019-08-02 17:18:25 +0200

    halbgarer code für djikstra, kein internet, deswegen aufgehört :(

--- program/environment/cars.py
+++ program/environment/cars.py
@@ -10,7 +10,14 @@ class Car:
 			self.actual_edge = edge_IDs[0]
 			self.future_edge_IDs = edge_IDs[1:]
 		########################################################
-	def djikstra(self): #calculates edge_IDs
+	def djikstra(self, start_node, end_node): #calculates edge_IDs
 		pass
-
+		#visited_nodes = [start_node]
+		#next_edges = []
+		#super_duper_way_of_djikstra_edge_IDs = []
+		#def get_next_vertex_ID(visited_nodes):
+			#next_edges.append(visited_nodes.connections) #iterieren!, Liste ordnen? oder einfach so lassen
+			#"return next_edges.connections.getlowestconnection" -->ordnen wäre für "getlowestconnection" besser oder so...
+		#while get_next_vertex_ID(visited_node_IDs) != end_node_ID:
+		#	visited_node_IDs.append(get_next_vertex_ID(visited_node_IDs))
 
