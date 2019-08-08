61b1ce2870e43e48f48fd6f68d922be371103bd0
Davanchama <davanchama@gmail.com>
2019-08-08 15:13:19 +0200

    prototyp einer funktion für knotengenerierung

--- program/simulation.py
+++ program/simulation.py
@@ -1,4 +1,13 @@
-
+import numpy as np
 
 #In dieser Datei sollen verschiedene Funktionen geschrieben werden, die eine realistische und praktische Simulation ermöglichen
+
+def generate_nodepositions_per_center(n_nodes): #generiert für jedes Zentrum die Positionen für die Knoten, Anzahl vorgegeben, Normalverteilung um Zentrum
+	node_positions_x = np.zeros(n_nodes) 
+	node_positions_y = np.zeros(n_nodes)
+	center_position_x = np.random.randint(low=0, high=50)
+	center_position_y = np.random.randint(low=0, high=50)
+	for i in range(n_nodes):
+		node_positions_x[i] = np.rand.normal(loc=center_position_x, scale=5)
+		node_positions_y[i] = np.rand.normal(loc=center_position_y, scale=5)
 
