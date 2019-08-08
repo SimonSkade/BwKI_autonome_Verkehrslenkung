8afd6515d64f6d45dc9e3d359156b17c0c1e6826
SimonSkade <simon.skade@gmx.net>
2019-08-08 23:41:48 +0200

    kleine Änderungen

--- program/simulation.py
+++ program/simulation.py
@@ -4,25 +4,28 @@ import matplotlib.pyplot as plt
 #In dieser Datei sollen verschiedene Funktionen geschrieben werden, die eine realistische und praktische Simulation ermöglichen
 
 def generate_nodepositions_per_center(n_nodes): #generiert für jedes Zentrum die Positionen für die Knoten, Anzahl vorgegeben, Normalverteilung um Zentrum, Ränder automatisch generiert
+	space_size = 1000
+	scale_radius = space_size / 20
 	node_positions_x = np.zeros(n_nodes, dtype=np.intc) #Arbeitsweise: x- und y-Werte werden getrennt dargestellt
-	node_positions_y = np.zeros(n_nodes, dtype=np.intc)
-	center_position_x = np.random.randint(low=0, high=50)
-	center_position_y = np.random.randint(low=0, high=50)
+	node_positions_y = np.zeros(n_nodes, dtype=np.intc) #Wenn das Zentrum sehr nah am Rand liegt, kommt es zu Problemen
+	center_position_x = np.random.randint(low=0, high=space_size)
+	center_position_y = np.random.randint(low=0, high=space_size)
 	node_positions = []
 	for i in range(n_nodes):
-		node_positions_x[i] = np.round(np.random.normal(loc=center_position_x, scale=5)) #möglicherweise Knoten außerhalb des Koordinatensystems, deswegen Z. 12-15
+		node_positions_x[i] = np.round(np.random.normal(loc=center_position_x, scale=scale_radius)) #möglicherweise Knoten außerhalb des Koordinatensystems, deswegen Z. 12-15
 		if node_positions_x[i] < 0:
 			node_positions_x[i] = 0
-		elif node_positions_x[i] > 50:
-			node_positions_x[i] = 50
-		node_positions_y[i] = np.round(np.random.normal(loc=center_position_y, scale=5)) #dasselbe für y-Werte
+		elif node_positions_x[i] > space_size:
+			node_positions_x[i] = space_size
+		node_positions_y[i] = np.round(np.random.normal(loc=center_position_y, scale=scale_radius)) #dasselbe für y-Werte
 		if node_positions_y[i] < 0:
 			node_positions_y[i] = 0
-		elif node_positions_y[i] > 50:
-			node_positions_y[i] = 50
+		elif node_positions_y[i] > space_size:
+			node_positions_y[i] = space_size
 		node_positions.append((int(node_positions_x[i]), int(node_positions_y[i])))
 	fig = plt.figure()
 	plt.scatter(node_positions_x, node_positions_y) #aus Interesse plotten
+	plt.scatter(center_position_x, center_position_y, color='red')
 	plt.show()
 	#Ich habe die Rückgabe geändert zu einer Liste von Knoten mit den Koordinaten
 	return node_positions #np.column_stack((node_positions_x, node_positions_y)) #gibt zweidimensionales Array zurück, eine Zeile alle x eine für alle y Koordinaten
