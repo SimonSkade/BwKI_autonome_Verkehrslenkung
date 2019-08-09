428b4bcef4eeef26285a834d7492eee67705c871
Davanchama <davanchama@gmail.com>
2019-08-09 13:47:06 +0200

    updated weights von edges

--- program/simulation.py
+++ program/simulation.py
@@ -25,8 +25,8 @@ def generate_nodepositions_per_center(n_nodes, space_size): #generiert für jede
         elif node_positions_y[i] > space_size:
             node_positions_y[i] = space_size
         node_positions.append((int(node_positions_x[i]), int(node_positions_y[i])))
-    plt.scatter(node_positions_x, node_positions_y) #aus Interesse plotten
-    plt.scatter(center_position_x, center_position_y, color='red')
+    #plt.scatter(node_positions_x, node_positions_y) #aus Interesse plotten
+    #plt.scatter(center_position_x, center_position_y, color='red')
     #Ich habe die Rückgabe geändert zu einer Liste von Knoten mit den Koordinaten
     return node_positions
 
@@ -39,7 +39,7 @@ def generate_random_nodes(n_nodes, space_size):
 		node_positions_x.append(np.random.randint(low=0.5*scale_radius, high=space_size-0.5*scale_radius))
 		node_positions_y.append(np.random.randint(low=0.5*scale_radius, high=space_size-0.5*scale_radius))
 		random_nodes.append((node_positions_x[i], node_positions_y[i]))
-	plt.scatter(node_positions_x, node_positions_y)
+	#plt.scatter(node_positions_x, node_positions_y)
 	return random_nodes
 
 def generate_border_nodes(n_border_nodes, space_size):
@@ -55,7 +55,7 @@ def generate_border_nodes(n_border_nodes, space_size):
             node_positions_y[i] = np.random.choice(2, 1)*space_size
             node_positions_x[i] = np.random.randint(low=0, high=space_size)
         node_positions.append((int(node_positions_x[i]), int(node_positions_y[i])))
-    plt.scatter(node_positions_x, node_positions_y) #aus Interesse plotten
+    #plt.scatter(node_positions_x, node_positions_y) #aus Interesse plotten
     return node_positions
 
 def generate_nodes(n_nodes, n_centers=4, space_size=1000):
@@ -69,7 +69,7 @@ def generate_nodes(n_nodes, n_centers=4, space_size=1000):
 	    node_positions1 += generate_nodepositions_per_center(abs(int(np.random.normal(loc=n_nodes/n_centers, scale=15))), space_size) #Testen (dann muss nur simulation.py aufgerufen werden)
 	node_positions2 = generate_random_nodes(n_spread_nodes, space_size)
 	node_positions3 = generate_border_nodes(n_border_nodes, space_size)
-	plt.show()
+	#plt.show()
 	node_positions = node_positions1 + node_positions2 + node_positions3
 	return node_positions
 
@@ -112,9 +112,25 @@ def generate_edges_without_weights(node_coordinates): #Vielleicht weights am End
 			if is_one_way < 0.95:
 				binary_edge_matrix[second_node, i] = 1
 			incoming = False if np.sum(binary_edge_matrix[:, i]) == 0 else True
-	return binary_edge_matrix
+	return binary_edge_matrix, distance_matrix
+
+def generate_weights_of_edges(binary_edge_matrix, distance_matrix):
+	a_matrix, b_matrix, c_matrix, d_matrix = np.zeros(binary_edge_matrix.shape), np.zeros(binary_edge_matrix.shape), np.zeros(binary_edge_matrix.shape), np.zeros(binary_edge_matrix.shape)
+	k = 30
+	g = 1
+	f = 30
+	h = 10
+	l = 0.15
+	for i in range(binary_edge_matrix.shape[0]):
+		for j in range(binary_edge_matrix.shape[0]):
+			if binary_edge_matrix[i, j] == 1:
+				a_matrix[i, j] = abs(np.random.normal(loc=h, scale=h/5)) * distance_matrix[i, j]
+				b_matrix[i, j] = abs(np.random.normal(loc=l, scale=l/5)) * distance_matrix[i, j]
+				c_matrix[i, j] = abs(np.random.normal(loc=f, scale=f/5)) * 1/(distance_matrix[i, j] + 1/3)
+				d_matrix[i, j] = np.sqrt(abs(np.random.normal(loc=k, scale=k/5)) * distance_matrix[i, j]) + abs(np.random.normal(loc=g, scale=g/5)) * distance_matrix[i, j]
+	return a_matrix, b_matrix, c_matrix, d_matrix
 
 
 node_positions = generate_nodes(100, 2)
-binary_edge_matrix = generate_edges_without_weights(node_positions)
-plot_edges(node_positions, binary_edge_matrix)
+binary_edge_matrix, distance_matrix = generate_edges_without_weights(node_positions)
+#plot_edges(node_positions, binary_edge_matrix)
