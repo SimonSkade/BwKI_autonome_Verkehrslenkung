e2c858821dfc210d35643aff083754bdc655c6a6
Davanchama <davanchama@gmail.com>
2019-08-29 20:58:39 +0200

    angefangen die netzstruktur zu programieren

--- program/Ki.py
+++ program/Ki.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-
+import torch
+import torch.nn as nn
+from environment.net import network
 #Hyperparameter
 EPOCHS = 100000
 ALPHA = 0.1
@@ -10,7 +11,15 @@ EPSILON_REDUCE_RATE = 0.95
 
 class KI:
 	def __init__(self):
-		pass
+		n_in = len(network.vertexes)
+		m_in = len(network.egdes)
+		n_hidden1 = 2*n_in
+		m_hidden1 = 2*m_in
+		hidden2 = 2*m_in
+		n_out = m_in
+		#model1 = nn.Sequential(OrderedDict([("fc1_n", nn.Linear(n_in, n_hidden1)), ("ReLU1_n", nn.ReLU())])) #für n
+		#model2 = nn.Sequential(OrderedDict([("fc1_m", nn.Linear(m_in, m_hidden1)), ("ReLU1_m", nn.ReLU())])) #für m
+		model3 = nn.Sequential(nn.Linear((n_in+m_in), (n_hidden1+m_hidden1), nn.ReLU(), nn.Linear((n_hidden1+m_hidden1), hidden2), nn.ReLU(), nn.Linear(hidden2, n_out), nn.Sigmoid()))
 
 	def train(self, alpha=ALPHA, epsilon=EPSILON, gamma=GAMMA, epochs=EPOCHS, alpha_reduce=ALPHA_REDUCE_RATE, epsilon_reduce=EPSILON_REDUCE_RATE):
 		for i in range(epochs):
