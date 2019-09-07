ec63fd972dd5e69990be297972c61aa292985873
Davanchama <davanchama@gmail.com>
2019-09-07 13:08:28 +0200

    erste schritte in die backpropagation und in richtung loss und in richtung optimizer

--- program/Ki.py
+++ program/Ki.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-#from environment.net import network
-#Hyperparameter
-EPOCHS = 100000
+from environment.net import network
+
+EPOCHS = 100
 ALPHA = 0.1
 EPSILON = 0.8
 GAMMA = 0.6
@@ -11,54 +11,57 @@ EPSILON_REDUCE_RATE = 0.95
 dtype = torch.float
 device = torch.device("cpu")
 
-class KI:
-    def __init__(self):
-        n_in = 3 #len(network.vertexes)
-        e_in = 5 #len(network.egdes)
+class KI():
+    def __init__(self, n_in, e_in):
+        #the params
+        self.n_in = n_in 
+        self.e_in = e_in 
         n_hidden1 = 2*n_in
         e_hidden1 = 2*e_in
         hidden2 = 2*e_in
         n_out = e_in
-        #model_nodes = nn.Sequential(OrderedDict([("fc1_n", nn.Linear(n_in, n_hidden1)), ("ReLU1_n", nn.ReLU())])) #für nodes
-        #model_edges = nn.Sequential(OrderedDict([("fc1_m", nn.Linear(m_in, m_hidden1)), ("ReLU1_m", nn.ReLU())])) #für edges
-        #model3 = nn.Sequential(nn.Linear((n_in+m_in), (n_hidden1+m_hidden1), nn.ReLU(), nn.Linear((n_hidden1+m_hidden1), hidden2), nn.ReLU(), nn.Linear(hidden2, n_out), nn.Sigmoid())) #the network
+        
+        #the models
+        self.model_n = nn.Sequential(nn.Linear(n_in, n_hidden1), nn.ReLU()) #für nodes
+        self.model_e = nn.Sequential(nn.Linear(e_in, e_hidden1), nn.ReLU()) #für edges
+        self.model2 = nn.Sequential(nn.Linear(n_hidden1+e_hidden1, hidden2), nn.ReLU(), nn.Linear(hidden2, n_out), nn.Sigmoid()) #für die zweite Hälfte
+        
+    def train(self, net_input, true_output, model, alpha=ALPHA, epsilon=EPSILON, gamma=GAMMA): #one train step
+        optimizer = torch.optim.Adam(model.parameters(), alpha) #the first magic line(optimizer)!
+        criterion = torch.nn.MSELoss() #the second magic line(loss)! ich benutze hier einen mean squared loss (siehe pytorch doc)
+    
+        #Trainingsprozess
+        #die KI berechnet ihren Output/forward
+        pred_output = model(net_input)
+        #Wir berechnen den Loss (deine Zeile, Lukas, ich hab hier den Standart-Loss berechnet, den die im Internet verwenden)
+        loss = criterion(pred_output, true_output) #hier ist der output gegeben, ich weiß nicht, ob das anwendbar bei uns ist, aber ich bin da ja nicht der Experte
+        print(' loss: ', loss.item())
+        #Adam kommt zum Einsatz
+        optimizer.zero_grad()
+        loss.backward()
+        #update die parameter
+        optimizer.step()
+        return model
+                
+    def forward(self, input_n, input_e, model_n, model_e, model2):
+        n_output = model_n(input_n)
+        e_output = model_e(input_e)
+        n_e_output = torch.cat((n_output, e_output), 0) #the output of the two nets are concatenated to one input for the other net
+        output = model2(n_e_output)
+        return output
+        
+        
+        
+ki = KI(len(network.vertexes), len(network.egdes))       
+n_in = torch.randn(ki.n_in)
+e_in = torch.randn(ki.e_in)
+model2_in = torch.randn(ki.n_in*2+ki.e_in*2)
+true_out = torch.randn(ki.e_in)
+#print(ki.forward(n_in, e_in, ki.model_n, ki.model_e, ki.model2))
+for i in range(EPOCHS):
+    ki.model2 = ki.train(model2_in, true_out, ki.model2)
 
-    def train(self,  input_nodes, input_edges, output, alpha=ALPHA, epsilon=EPSILON, gamma=GAMMA, epochs=EPOCHS, alpha_reduce=ALPHA_REDUCE_RATE, epsilon_reduce=EPSILON_REDUCE_RATE):
-        for i in range(epochs):
-            #Trainingsprozess
-
-
-            if i % 1000 == 0:
-                #Lern- und Zufallsrate anpassen
-                ALPHA *= alpha_reduce
-                EPSILON *= epsilon_reduce
-
-    def forward(input_nodes, input_edges, epochs=EPOCHS):
-        n_in_hid1_weights = torch.randn(n_in, n_hidden1, device=device, dtype=dtype)
-        #print("node weights:" , node_weights)
-        e_in_hid1_weights = torch.randn(e_in, e_hidden1, device=device, dtype=dtype)
-        #print("egde weights:" , egde_weights)
-        hid_1_2_weights = torch.randn(n_hidden1 + e_hidden1, hidden2, device=device, dtype=dtype)
-        hid2_out_weights = torch.randn(hidden2, n_out)
-        for i in range(1):
-            hidden1_n = input_nodes.matmul(n_in_hid1_weights)
-            #print("hidden1_n: " , hidden1_n)
-            hidden1_e = input_edges.matmul(e_in_hid1_weights)
-            #print("hidden1_e: " , hidden1_e)
-            hidden1_n_relu = hidden1_n.clamp(min=0)
-            hidden1_e_relu = hidden1_e.clamp(min=0)
-            hidden_1_out = torch.cat((hidden1_n_relu, hidden1_e_relu)) #the outputs of the node and egde nets are now concatenated
-            #print(hidden_1_out)
-            hidden2_in = hidden_1_out.matmul(hid_1_2_weights)
-            #print(hidden2_in)
-            hidden2_relu = hidden2_in.clamp(min=0)
-            out_in = hidden2_relu.matmul(hid2_out_weights)
-            out_out = torch.sigmoid(out_in)
-            print(out_out) #return out_out
-            
-
-nodes_in = torch.randn(n_in, device=torch.device("cpu"), dtype=torch.float) #create random inputs for testing
-#print("nodes in: " , nodes_in)
-edges_in = torch.randn(e_in, device=torch.device("cpu"), dtype=torch.float) #each for nodes and edges
-#print("edges_in: ", edges_in)
-test(nodes_in, edges_in)
+    if i % 1000 == 0:
+        #Lern- und Zufallsrate anpassen
+        ALPHA *= ALPHA_REDUCE_RATE
+        EPSILON *= EPSILON_REDUCE_RATE
